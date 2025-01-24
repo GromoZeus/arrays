b51491535870fc60b21833e38b5d7398e15680f8
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-01-24 20:08:03 +0700

    task1 done

---
+++ bin/task1.js
@@ -0,0 +1,12 @@
+const hammingWeight = (number) => {
+  const binaryStr = number.toString(2);
+  let result = 0;
+  for (let i = 0; i < binaryStr.length; i += 1) {
+    result += binaryStr[i] === '1' ? 1 : 0;
+  }
+  return result;
+};
+
+export default hammingWeight;
+
+console.log(hammingWeight(101));
