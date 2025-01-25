aebb3ae123d1b512f41376fe0fd02780e7524497
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-01-25 22:54:23 +0700

    task5 done

---
+++ bin/task5.js
@@ -0,0 +1,11 @@
+const chunkArray = (array, size) => {
+    const result = [];
+    while (array.length > 0) {
+      result.push(array.splice(0, size));
+    }
+    return result;
+  }
+  
+  const array = [1, 2, 3, 4, 5, 6, 7];
+  const chunked = chunkArray(array, 3);
+  console.log(chunked); // [[1, 2, 3], [4, 5, 6], [7]]
