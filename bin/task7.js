b50fa841b0a2e0d2eaf10c1a78e1e9ca2b18e0c1
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-01-26 17:16:34 +0700

    task7 done

---
+++ bin/task7.js
@@ -0,0 +1,39 @@
+const rotateLeft = (array) => {
+  const newArray = [];
+  for (let i = array[0].length - 1; i >= 0; i -= 1) {
+    const rowsArray = [];
+    for (let j = 0; j < array.length; j += 1) {
+      rowsArray.push(array[j][i]);
+    }
+    newArray.push(rowsArray);
+  }
+  return newArray;
+};
+
+const rotateRight = (array) => {
+    const newArray = [];
+    for (let i = 0; i < array[0].length; i += 1) {
+      const rowsArray = [];
+      for (let j = array.length - 1; j >= 0; j -= 1) {
+        rowsArray.push(array[j][i]);
+      }
+      newArray.push(rowsArray);
+    }
+    return newArray;
+  };
+
+console.log(
+  rotateLeft([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, 1, 2],
+  ]),
+);
+
+console.log(
+    rotateRight([
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 0, 1, 2],
+    ]),
+  );
