067bcbd40cdbef54a2978592a6224e481dc685fe
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-01-26 14:04:59 +0700

    task6 done

---
+++ bin/task6.js
@@ -0,0 +1,23 @@
+const getMirrorMatrix = (array) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    const newArray1 = [];
+    const newArray2 = [];
+    for (let j = 0; j < array[i].length / 2; j += 1) {
+      newArray1.push(array[i][j]);
+      newArray2.push(array[i][array[i].length / 2 - 1 - j]);
+    }
+    newArray1.push(...newArray2);
+    newArray.push(newArray1);
+  }
+  return newArray;
+};
+
+console.log(
+  getMirrorMatrix([
+    [11, 12, 13, 14],
+    [21, 22, 23, 24],
+    [31, 32, 33, 34],
+    [41, 42, 43, 44],
+  ]),
+);
