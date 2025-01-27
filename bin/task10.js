6f39a63ff23cf7549e84c57da8f6e558d4585c6d
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-01-27 21:26:35 +0700

    task10 done

---
+++ bin/task10.js
@@ -0,0 +1,23 @@
+const summaryRanges = (arr) => {
+  if (arr.length === 0) return [];
+  const result = [];
+  let start = arr[0];
+  let end = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1] + 1) {
+      end = arr[i];
+    } else {
+      if (start !== end) {
+        result.push(`${start}->${end}`);
+      }
+      start = arr[i];
+      end = arr[i];
+    }
+  }
+  if (start !== end) {
+    result.push(`${start}->${end}`);
+  }
+  return result;
+};
+
+console.log(summaryRanges([1, 1, 3, 4, 5, -6, 8, 9, 10, 12, 14, 14]));
